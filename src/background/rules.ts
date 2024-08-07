00302460d4b80e87f0a3de3ffe2a72b583e8f3d4
lohansimpson <lmichaelson07@gmail.com>
2024-08-07 01:56:16 -0500

    got headers working

---
+++ src/background/rules.ts
@@ -0,0 +1,42 @@
+const allResourceTypes = Object.values(chrome.declarativeNetRequest.ResourceType);
+
+const rules: chrome.declarativeNetRequest.Rule[] = [
+  {
+    id: 1,
+    priority: 1,
+    action: {
+      type: chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
+      requestHeaders: [
+        {
+          operation: chrome.declarativeNetRequest.HeaderOperation.SET,
+          header: 'user-agent',
+          value: 'Mozilla/5.0 (Linux; Android 11; Android TV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Mobile Safari/537.36',
+        },
+      ]
+    },
+    condition: {
+      urlFilter: '||x.com',
+      resourceTypes: allResourceTypes,
+    }
+  },
+  {
+    id: 2,
+    priority: 1,
+    action: {
+      type: chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
+      responseHeaders: [
+        {
+          operation: chrome.declarativeNetRequest.HeaderOperation.SET,
+          header: 'x-modifed-headers',
+          value: 'true',
+        },
+      ]
+    },
+    condition: {
+      urlFilter: '||x.com',
+      resourceTypes: allResourceTypes,
+    }
+  },
+];
+
+export default rules;
