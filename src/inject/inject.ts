cb89b87aaf98e7d677ca0dbffdcd139a06217966
lohansimpson <lmichaelson07@gmail.com>
2024-10-12 14:03:06 -0500

    ui / created button - added styles

--- src/inject/inject.ts
+++ src/inject/inject.ts
@@ -1,7 +1,10 @@
 import { createObserver } from "./dom/createObserver";
 
 const showModal = () => {
+    const btnContainer = document.querySelector(".featuresBtnContainer");
+    btnContainer?.classList.add("active");
     const background = document.createElement("div");
+    
     background.classList.add("modalBackground");
     background.onclick = closeModal;
 
@@ -11,6 +14,8 @@ const showModal = () => {
 
     background.appendChild(iframe);
     document.body.appendChild(background);
+    btnContainer?.classList.remove("active");
+
 };
 
 const closeModal = () => {
@@ -56,10 +61,52 @@ const onMenuAdded = async (prevElement: Element) => {
     if (prefersDarkMode) {
         btnContainer.classList.add("dark");
     }
-    label.onclick = showModal;
+    label.onclick = (e) => {
+        e.preventDefault();
+        btnContainer.classList.toggle("active");
+        showModal();
+    };
+
 
     btnContainer.append(label);
     prevElement.after(btnContainer);
+
+    // Add new button
+
+
+    const img_2 = document.createElement("img");
+
+    img_2.src = prefersDarkMode
+        ? chrome.runtime.getURL("assets/icons/tv_icon_dark_theme.svg")
+        : chrome.runtime.getURL("assets/icons/tv_icon_light.svg");
+    img_2.classList.add("TVIcon");
+
+
+    const newButton = document.createElement("div");
+    newButton.classList.add("newFeatureBtn");
+    if (prefersDarkMode) {
+        newButton.classList.add("dark");
+    }
+    newButton.append(img_2);
+
+    const newButtonText = document.createElement("span");
+    newButtonText.innerText = "Tv Mode";
+    newButton.append(newButtonText);
+
+    const btnContainer_2 = document.createElement("div");
+    btnContainer_2.classList.add("newBtnContainer");
+    if (prefersDarkMode) {
+        btnContainer_2.classList.add("dark");
+    }
+    newButton.onclick = (e) => {
+        e.preventDefault();
+        // Add your new feature functionality here
+        console.log("New feature button clicked");
+    };
+
+    // Insert the new button after the btnContainer
+    btnContainer_2.append(newButton);
+    btnContainer.after(btnContainer_2);
 };
 
 localStorage.setItem("stf_extension_id", chrome.runtime.id);
